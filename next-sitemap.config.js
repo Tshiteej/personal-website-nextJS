3ff4fa3c5a6b04ea087913fa51ce3b553cdd0b52
apple <apple@FVFZF8A4L410.local>
2023-07-30 00:59:35 +0530

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
 module.exports = {
   siteUrl,
-  exclude: ["/404"],
+  exclude: ["/404", "/server-sitemap.xml"],
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
@@ -11,21 +11,20 @@ module.exports = {
       },
       { userAgent: "*", allow: "/" },
     ],
-    additionalSitemaps: [
-    ],
+    additionalSitemaps: [],
   },
   transform: async (config, path) => {
-    console.log({config, path})
-    let isHomePage = path == "/" ? true : false
-    let isBlogPage = path == "/blog" ? true : false
-    let changefreq = isHomePage ? "monthly" : isBlogPage ? "weekly" : "yearly"
-    let priority = isHomePage ? 1 : 0.9
+    console.log({ config, path });
+    let isHomePage = path == "/" ? true : false;
+    let isBlogPage = path == "/blog" ? true : false;
+    let changefreq = isHomePage ? "monthly" : isBlogPage ? "weekly" : "yearly";
+    let priority = isHomePage ? 1 : 0.9;
     return {
       loc: path,
       changefreq,
       priority,
       lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
       alternateRefs: config.alternateRefs ?? [],
-    }
+    };
   },
 };
